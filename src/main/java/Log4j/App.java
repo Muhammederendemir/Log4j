4f982b62fa98f4a7f152ec1c42f3afdcffd3d198
Muhammed Eren Demir <mhmmderen3@gmail.com>
2019-04-18 10:35:08 +0300

    Açıklama dosyası ve XML dosyası eklendi.

--- src/main/java/Log4j/App.java
+++ src/main/java/Log4j/App.java
@@ -1,13 +1,15 @@
 package Log4j;
 
 import org.apache.log4j.Logger;
- import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.xml.DOMConfigurator;
 
 public class App
 {  static  final Logger LOGGER = Logger.getLogger(App.class);
     public static void main( String[] args )
     {
-        PropertyConfigurator.configure("C:\\Users\\Win10\\Desktop\\Log4jExample4\\src\\main\\resources\\log4j.properties");
+        //PropertyConfigurator.configure("C:\\Users\\Win10\\Desktop\\Log4jExample4\\src\\main\\resources\\log4j.properties");
+        DOMConfigurator.configure("C:\\Users\\Win10\\Desktop\\Log4jExample4\\src\\main\\resources\\log4j.xml");
         LOGGER.debug("debug message");
         LOGGER.info("info message");
         LOGGER.warn("warn message");
